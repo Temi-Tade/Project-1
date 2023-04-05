a368c2209622f89edbcec212e17c33243c8da853
akintadetemi19@gmail.com <akintadetemi19@gmail.com>
2023-04-05 22:19:18 +0100

    more ui, svg

--- JavaScripts/Load.js
+++ JavaScripts/Load.js
@@ -2,18 +2,18 @@ let progress = document.querySelector('progress')
 let val = progress.value
 
 document.querySelector('#loadcon').style.display = 'flex'
-document.querySelector('.container').style.opacity = '0'
+document.querySelector('.container').style.display = 'none'
 
 let t = setInterval(() => {
 	if (progress.value === 100) {
 		clearInterval(t)
 		document.querySelector('#loadcon').style.display = 'none'
-		document.querySelector('.container').style.opacity = '1'
+		document.querySelector('.container').style.display = 'block'
 		document.onreadystatechange = () => {
 			if (document.readyState !== 'complete') {
-				document.querySelector('.container').style.opacity = '0'
+				document.querySelector('.container').style.display = 'none'
 			} else {
-				document.querySelector('.container').style.opacity = '1'
+				document.querySelector('.container').style.display = 'block'
 			}
 		}
 	} else {
